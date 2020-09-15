147f7034542192598eb3c517aeb48545e4eceb5d
Mohammad ghazal <moh.g.ghazal@gmail.com>
2020-09-16 01:25:40 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,15 @@ const cellClick =function(id){
  //document.querySelector('#c17').style.background = 'red'
 
 }
+const dayClick =function(id){
+  //console.log("cell Clicked")
+ // console.log( document.querySelector("#"+id).innerText)
+ console.log(id)
+ id.style.background = 'blue'
+//document.querySelector("'#"+id+"'").style.background = 'red' 
+//document.querySelector('#c17').style.background = 'red'
 
+}
 
 const createCalender =function () {
     let x = document.createElement("TABLE");
@@ -26,18 +34,24 @@ const createCalender =function () {
     // z.appendChild(t);
    //document.getElementById("myTr").appendChild(z);
    let table = document.getElementById("myTable");
-
+   let y=0;
 // Create an empty <tr> element and add it to the 1st position of the table:
-for(x=0;i=x<=7;x++){
+let week=["SAT","SUN","MON","TUE","WED","THU","FRI"]       
+for(x=0;i=x<=6;x++){
 let row = table.insertRow(-1); // -1 mean insert befor
 // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-let l=9;
+ l=9;
 for(i=0;i<=9;i++){
-    let cell = row.insertCell(i);
+  let cell = row.insertCell(i);
+    if(i===0){
+      cell.innerHTML = week[y++];
+      cell.setAttribute("id","d"+x+""+i);
+      cell.setAttribute("onClick", "dayClick(d"+x+""+i+")") 
+    }else{
     cell.innerHTML = l++ +":00 "//+x+""+i;
     cell.setAttribute("id","c"+x+""+i);
     cell.setAttribute("onClick", "cellClick(c"+x+""+i+")") 
-
+  }
     //cell.setAttribute("onclick",cellClick(cell.id))
 }}
 
@@ -49,7 +63,15 @@ for(i=0;i<=9;i++){
 // cell1.innerHTML = "NEW CELL1";
 // cell2.innerHTML = "NEW CELL2";
 }
+
+
 createCalender();
+
+
+
+
+
+
 // 1. when click on #add do S2
 
 // 2. get the text inside the #task
